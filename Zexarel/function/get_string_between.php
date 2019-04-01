46e76e859b6fcb0cd0b09d7ba91422a4ef509beb
Zexal0807 <gallinar00@gmail.com>
2019-04-01 12:13:22 +0200

    Remove Zexarel

--- Zexarel/function/get_string_between.php
+++
@@ -1,6 +0,0 @@
-<?php
-function get_string_between($str, $from, $to){
-	$sub = substr($str, strpos($str, $from) + strlen($from), strlen($str));
-	return substr($sub, 0, strpos($sub, $to));
-}
-?>
--- Zexarel/function/redirect.php
+++
@@ -1,5 +0,0 @@
-<?php
-	function redirect($str){
-		header("Location: ".$str);
-	}
-?>

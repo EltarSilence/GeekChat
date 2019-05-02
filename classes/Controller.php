4b99586330e6de055521088d81af0112ca843fef
Zexal0807 <gallinar00@gmail.com>
2019-05-02 09:25:40 +0200

    Modificati script rimuovend database

--- classes/Controller.php
+++ classes/Controller.php
@@ -1,17 +1,20 @@
 <?php
 class Controller{
   public static function getProfile($id){
-  	$j = array(); //array associativo di risposta
+  	$j = array();            //array associativo di risposta
   	if (isset($id) && !empty($id)){
-      $db = new Database();
-      $data = $db->selectAll()
-        ->from("utenti")
-        ->where("id", "=", $id)
-        ->execute();
-  		if(sizeof($data) == 0){   //no user found
+      $mysqli = new mysqli("localhost", "root", "", "geekchat");
+      if($mysqli->connect_errno){
+      	die();
+      }
+      $id = isset($_GET['id']) ? $_GET['id'] : 0;
+      $result = $mysqli->query("SELECT * FROM utenti WHERE id =".$id);
+      $result = $conn->query($sql);   //esecuzione query
+  		$data = $result->fetch_assoc(); //da obj a arr associativo
+  		if($result->num_rows == 0){     //no user found
   			$j['error'] = true;
   			$j['errorMSG'] = "Non esistono utenti con questo ID";
-  		}else{ //utente trovato -> assegnazione dati
+  		}else{                          //utente trovato -> assegnazione dati
   			$j['username'] = $data['username'];
   			$j['isAdmin'] = $data['isAdmin'];
   			$j['bio'] = $data['bio'];
@@ -19,7 +22,7 @@ class Controller{
   			$j['lastAccess'] = $data['lastAccess'];
   			$j['error'] = false;
   		}
-  	}else{  //manca il parametro
+  	}else{                            //manca il parametro
   		$j['error'] = true;
   		$j['errorMSG'] = "Nessun ID rilevato";
   	}

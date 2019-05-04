007b34d8ff9eba5f1fd39e0f5f647076692ac056
Zexal0807 <gallinar00@gmail.com>
2019-05-04 16:04:12 +0200

    Add user model

---
+++ php_model/user.php
@@ -0,0 +1,21 @@
+<?php
+class User extends ZModel{
+  public $dir = "../model/";
+  public $model = "user";
+/*
+  private $nome;
+  private $cognome;
+  private $username;
+  private $immagine;
+  private $biografia;
+
+  public function __contruct($nome, $cognome, $username, $immagine, $biografia){
+    $this->nome = $nome;
+    $this->cognome = $cognome;
+    $this->username = $username;
+    $this->immagine = $immagine;
+    $this->biografia = $biografia;
+  }
+*/
+}
+?>

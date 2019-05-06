999b1e103e94addb3016e9864bb81c48318e2035
Zexal0807 <gallinar00@gmail.com>
2019-05-06 13:18:49 +0200

    Add view in index

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'Zexarel/loader.php';
 
+require_once 'classes/AppView.php';
 require_once 'classes/Controller.php';
 require_once 'php_model/user.php';
 
@@ -9,9 +10,7 @@ ZRoute::get("/", function (){
 });
 
 ZRoute::get("/home", function (){
-  //echo "Questa è la route della homepage";
-  $u = new User(["nome"=>"Roberto", "cognome"=>"Gallina", "username"=>"Zexal0807", "biografia"=>"Questa è la bio"]);
-  echo $u->getHtml();
+  AppView::get("home");
 }, "home");
 
 /*           CHIAMATE AJAX            */

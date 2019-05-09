1bfb14769515013cb8cb65decf9e0b884f3d7138
Giovanni Rizza <rizza67jorge@gmail.com>
2019-05-09 09:06:48 +0200

    Sistemazione row&toggle-chevron jQuery

--- messageBar/msg.js
+++ messageBar/msg.js
@@ -1,13 +1,18 @@
 $( document ).ready(function() {
-    $("#row2").toggle();
+  $("#row2").toggle();
 	$("#row3").toggle();
 });
 
 $("#coll").on("click", function(){
 	$("#row2").toggle();
+  if ($('#row3').is(":visible")){
+    $('#row3').toggle();
+    $('#row2').find('i').toggleClass('fa-chevron-down fa-chevron-up');
+  }
+  $(this).find('i').toggleClass('fa-chevron-down fa-chevron-up');
 });
 
 $("#coll2").on("click", function(){
 	$("#row3").toggle();
+  $(this).find('i').toggleClass('fa-chevron-down fa-chevron-up');
 })
-

151fe522294e96139f931ae291aee938ab784925
Zexal0807 <gallinar00@gmail.com>
2019-05-18 09:44:28 +0200

    Update messageBar

--- assets/js/messageBar.js
+++ assets/js/messageBar.js
@@ -1,16 +1,25 @@
 $(document).ready(function(){
   $("#coll").click(function(){
-  	$("#row2").toggle();
-    if ($('#row3').is(":visible")){
-      $('#row3').toggle();
-      $('#row2').find('i').toggleClass('fa-chevron-down fa-chevron-up');
+    if ($('#row2').is(":visible")){
+      $(this).find('i').removeClass('fa-chevron-up');
+      $(this).find('i').addClass('fa-chevron-down');
+      $("#row2").slideUp(400);
+      if ($('#row3').is(":visible")){
+        $("#row3").slideUp(400);
+      }
+    }else{
+      $(this).find('i').removeClass('fa-chevron-down');
+      $(this).find('i').addClass('fa-chevron-up');
+      $("#row2").slideDown(400);
     }
-    $(this).find('i').toggleClass('fa-chevron-down fa-chevron-up');
   });
 
   $("#coll2").click(function(){
-  	$("#row3").toggle();
-    $(this).find('i').toggleClass('fa-chevron-down fa-chevron-up');
+    if ($('#row3').is(":visible")){
+      $("#row3").slideUp(400);
+    }else{
+      $("#row3").slideDown(400);
+    }
   });
 
 });

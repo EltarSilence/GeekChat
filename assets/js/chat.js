d446e7d491077eb0961e702108f9a2c55ae8416b
Zexal0807 <gallinar00@gmail.com>
2019-05-30 09:12:20 +0200

    Add ajax for check new message

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
-  $.ajax({
-    url : "getMessage",
-    method : "POST",
-    data : {
-      id : "1"
-    },
-    success: function(data){
-      $('#chat > div:first-child').append(data);
-    },
-    error: function(er){
-      console.log(er);
-    }
-  });
+  var int = setIntervall(function(){
+    $.ajax({
+      url : "getMessage",
+      method : "POST",
+      data : {
+        id : $('#chat > div:first-child > div:last-child').attr('data-id')
+      },
+      success: function(data){
+        $('#chat > div:first-child').append(data);
+      },
+      error: function(er){
+        console.log(er);
+      }
+    });
+  }, 1500);
 });
